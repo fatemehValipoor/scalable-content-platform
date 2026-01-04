af4b28f82fadb248d2d8b1be6b5e33a5d5a9e23c
f.valipoor <f.valipoor24@gmail.com>
2026-01-05 00:23:39 +0330

    refactor(core): extract request and response utilities

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const { createServer } = require("node:http");
-console.log("1");
 const serverConfig = require("./config/server.config");
-console.log("2");
 const app = require("./app");
-console.log("3");
 
 const server = createServer(app);
 
